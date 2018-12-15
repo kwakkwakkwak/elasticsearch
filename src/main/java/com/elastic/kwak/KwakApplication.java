e9f468343574e8cfa8eb6e6ceed55545f0df407a
[지나웍스] 곽경진 <kwakkj112@zinnaworks.com>
2018-12-15 14:07:46 +0900

    frist commit

---
+++ src/main/java/com/elastic/kwak/KwakApplication.java
@@ -0,0 +1,14 @@
+package com.elastic.kwak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KwakApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KwakApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/elastic/kwak/KwakApplicationTests.java
@@ -0,0 +1,17 @@
+package com.elastic.kwak;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KwakApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
